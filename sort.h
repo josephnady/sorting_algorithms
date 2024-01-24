791fbe30399472ac90972e8352fb4d2a4736ed51
josephnady <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@github.com>
2024-01-23 16:48:01 -0800

    insertion algorithm

--- sort.h
+++ sort.h
@@ -17,9 +17,13 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-
+/* --------- Util ---------- */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/* --------- Algorithm function ---------- */
 void bubble_sort(const int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+
 
 #endif /* SORT_H */
